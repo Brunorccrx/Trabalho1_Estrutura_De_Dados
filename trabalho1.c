c72b691bc114051d3efb096a5103d412139355f7
Brunorccrx <brunorccrx@gmail.com>
2016-12-27 16:45:27 -0400

    Add files via upload

--- trabalho1.c
+++ trabalho1.c
@@ -30,24 +30,55 @@ Dicionario *definirDicionario(char *string, Dicionario *dicionario){
 	int linhaAtual=0;
 	char auxPalavra[20]={0};
 	Dicionario *d;
+	Palavra *nova;
+
 	for(i=0;i<strlen(string)-4;){//strlen está considerando vazios e lixo por isso "-4"
-		while(string[i]!=' '|| string[i]!='\n'){
+		while(string[i]!=' '){
 			auxPalavra[i]=string[i];
 			i++;
 		}
-		while(string[i]==' '|| string[i]=='\n')
+		while(string[i]==' ')
 			i++;
 
-		if(buscaPalavra(dicionario,auxPalavra)==NULL){
+		d=buscaPalavra(dicionario,auxPalavra);
+		if(d==NULL){
 			Dicionario *novo=(Dicionario *)malloc(sizeof(Dicionario *));
 			Palavra *nova=(Palavra *)malloc(sizeof(Palavra *));
+			novo->proxP=NULL;
+			nova->linha=linhaAtual;
+			nova->repeticoes=1;
 
 			strcpy(auxPalavra,novo->palavra);
 
 			novo->proxD=dicionario;
 			dicionario = novo;
 
+			nova->prox=novo->proxP;
+			novo->proxP=nova;
+		}
+		else{
+			if(linhaAtual==d->proxP->linha)
+				d->proxP->repeticoes++;
+			else{
+				Palavra *nova=(Palavra *)malloc(sizeof(Palavra *));
+				nova->linha=linhaAtual;
+				nova->repeticoes=1;
+
+				nova->prox=d->proxP;
+				d->proxP=nova;
+			}
+		}
+		if(string[i]=='\n'){
+			linhaAtual++;
+			i++;
 		}
 	}
 	return novo;
+}
+Palavra *iniciarPalavra(){
+	Palavra *nova=(Palavra *)malloc(sizeof(Palavra *));
+	nova->linha=linhaAtual;
+	nova->repeticoes=1;
+
+	return nova;
 }
